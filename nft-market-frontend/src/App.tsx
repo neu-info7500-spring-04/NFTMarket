03c1676799e9fa87363568463d2222f694691942
Danielnewheart <Grace600522abc>
2024-04-15 12:34:14 -0400

    Feat: added get erc20 component

--- nft-market-frontend/src/App.tsx
+++ nft-market-frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { MintNFT } from './MintNFT';
+import { MintERC20 } from './MintERC20';
 import './index.css';
 
 function App() {
-  const CONTRACT_ADDRESS = "0x8Ca68D231555c79E34dEfc769a8b9529312C5711";
+  const CONTRACT_ADDRESS = "0x85FAD331426db7fdb82E4EAdE05Bae889fC44695";
+  const TOKEN_CONTRACT_ADDRESS = "0x49fBFE1517b34D9eFd01F9e37A9400B2e00AA376"
+
   const account = useAccount()
   const { connectors, connect, error } = useConnect()
   const { disconnect } = useDisconnect()
@@ -46,7 +49,8 @@ function App() {
           {/* <div>{status}</div> */}
           <div>{error?.message}</div>
         </div>}
-
+        
+        {account.status === 'connected' && <MintERC20 tokenContractAddress={TOKEN_CONTRACT_ADDRESS}/>}
         {account.status === 'connected' && <MintNFT address={account.address} contractAddress={CONTRACT_ADDRESS} />}
       </div>
     </>
